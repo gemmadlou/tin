f27a7d7234abcdd9a3088bb11bc4a03f915cee9b
Gemma Black <gblackuk@googlemail.com>
2023-12-02 21:49:04 +0000

    [Prototype] add schema listings table to schema page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,44 @@
 <template>
     <div class="container mx-auto p-5">
         <h1 class="text-xl">Schemas</h1>
+
+        <div class="h-10"></div>
+
+        <div class="grid grid-cols-4">
+            <div class="col-span-2">
+                <table class="table table-zebra">
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Name</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="schema in schemas">
+                            <th>{{ schema.id }}</th>
+                            <td>{{ schema.name }}</td>
+                            <td><button @click="viewSchema(schema)" class="btn btn-outline btn-xs">View</button></td>
+                            <td><button class="btn btn-outline btn-xs btn-disabled">Delete</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
     View prototype <nuxt-link to="/prototype">here</nuxt-link>.
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import * as model from "../src/models";
+
+let schemas = ref<model.Schema[]>([]);
+schemas = (await useFetch('/api/schema')).data.value
+
+let viewSchema = (schema: model.Schema) => {
+    alert('View schema')
+}
+</script>
---
+++ src/models.ts
@@ -0,0 +1,7 @@
+export type json = string;
+
+export type Schema = {
+    id: number,
+    name: string,
+    json: json
+}
