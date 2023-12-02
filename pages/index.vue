648d0166e1a2eb61c23ab325ebf980059fb58f39
Gemma Black <gblackuk@googlemail.com>
2023-12-02 21:32:08 +0000

    [Prototype] create schema page heading section

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
 <template>
+    <div class="container mx-auto p-5">
+        <h1 class="text-xl">Schemas</h1>
+    </div>
+
     View prototype <nuxt-link to="/prototype">here</nuxt-link>.
 </template>
