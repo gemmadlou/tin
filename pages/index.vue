d72cb158d027ed922ad6bca8e4f38c723637e770
Gemma Black <gblackuk@googlemail.com>
2023-12-02 22:29:23 +0000

    [Prototype] show schema details when selected

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,6 @@
                             <td>ID</td>
                             <td>Name</td>
                             <td></td>
-                            <td></td>
                         </tr>
                     </thead>
                     <tbody>
@@ -20,13 +19,41 @@
                             <th>{{ schema.id }}</th>
                             <td>{{ schema.name }}</td>
                             <td><button @click="viewSchema(schema)" class="btn btn-outline btn-xs">View</button></td>
-                            <td><button class="btn btn-outline btn-xs btn-disabled">Delete</button></td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-            <div class="col-span-2">
-                <button v-on:click="createSchema" class="btn btn-primary">Create new schema</button>
+            <div class="col-span-2 bg-slate-50 p-10">
+                <button v-if="!canViewForm" v-on:click="createSchema" class="btn btn-primary">Create new schema</button>
+                <div v-else class="text-right">
+                    <button v-on:click="closeSchemaForm" class="btn btn-error text-white">Close</button>
+                </div>
+
+                <form v-if="canViewForm" @submit.prevent="createSchema">
+                    <input v-if="form.id" type="hidden" v-model="form.id" />
+
+                    <div class="form-control mb-5">
+                        <label class="label-text">Schema name</label>
+                        <input v-model="form.name" type="text" placeholder="Type here"
+                            class="input input-bordered w-full max-w-xs" />
+                    </div>
+
+                    <div class="form-control mb-5">
+                        <label class="label">
+                            <span class="label-text">Create schema</span>
+                            <a href="https://codebeautify.org/json-to-json-schema-generator" target="_blank">
+                                <span class="label-text-alt">JSON Schema</span>
+                            </a>
+                        </label>
+                        <textarea v-model="form.json" class="textarea textarea-bordered h-80 font-mono text-xs"
+                            placeholder="JSON Schema"></textarea>
+                    </div>
+                    <div class="text-right">
+                        <button v-if="!form.id" class="btn btn-primary">Create Schema</button>
+                        <button v-else class="btn btn-primary">Edit Schema</button>
+                    </div>
+
+                </form>
             </div>
         </div>
     </div>
@@ -42,10 +69,34 @@ let schemas = ref<model.Schema[]>([]);
 schemas = (await useFetch('/api/schema')).data.value
 
 let viewSchema = (schema: model.Schema) => {
-    alert('View schema')
+    canViewForm.value = true
+    form.value.id = schema.id
+    form.value.name = schema.name
+    form.value.json = schema.json
 }
+
+let canViewForm = ref(false);
+
+type SchemaForm = {
+    id: number | undefined,
+    name: string | undefined,
+    json: string | undefined
+}
+
+let form = ref<SchemaForm>({
+    id: undefined,
+    name: undefined,
+    json: undefined
+})
 
 let createSchema = () => {
     alert('Create schema')
+}
+
+let closeSchemaForm = () => {
+    canViewForm.value = false
+    form.value.id = undefined
+    form.value.name = undefined
+    form.value.json = undefined
 }
 </script>
