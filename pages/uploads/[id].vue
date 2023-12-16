db74149f50a65dbe940bbc6de39e975441bdf4f6
Gemma Black <gblackuk@googlemail.com>
2023-12-16 17:06:33 +0000

    [Prototype] create basic page sections for uploads page

--- pages/uploads/[id].vue
+++ pages/uploads/[id].vue
@@ -1,3 +1,25 @@
 <template>
-    Uploads
+    <Header />
+
+    <div class="container mx-auto p-5">
+        <h1 class="text-xl">Import your data file</h1>
+
+        <div class="h-10"></div>
+
+        <div>
+            Step 1 - Upload file
+        </div>
+
+        <div class="h-10"></div>
+
+        <div>
+            Step 2 - Map file
+        </div>
+
+        <div class="h-10"></div>
+
+        <div>
+            Step 3 - Import file
+        </div>
+    </div>
 </template>
