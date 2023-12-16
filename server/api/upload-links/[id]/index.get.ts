777b77f7a0872299f9bd67b81d7dd5443b8ae510
Gemma Black <gblackuk@googlemail.com>
2023-12-16 17:41:01 +0000

    [Prototype] look up upload link also by uuid
    See source of UUID validation regex from https://github.com/nodejs/node/issues/46748#issuecomment-1440838404

--- server/api/upload-links/[id]/index.get.ts
+++ server/api/upload-links/[id]/index.get.ts
@@ -1,12 +1,25 @@
 import { connection } from "~/src/mysql"
 
+const UUID_REGEX = /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i;
+
+const isUuid = (uuid : unknown) => typeof uuid === 'string' && UUID_REGEX.test(uuid);
+
 export default defineEventHandler(async (event) => {
     let conn = await connection()
 
-    let [response] = await conn.query(
-        'select * from `schema_upload_links` where id = ?',
-        [event.context?.params?.id]
-    )
+    let response;
+
+    if (isUuid(event.context?.params?.id)) {
+        [response] = await conn.query(
+            'select * from `schema_upload_links` where uuid = ?',
+            [event.context?.params?.id]
+        )
+    } else {
+        [response] = await conn.query(
+            'select * from `schema_upload_links` where id = ?',
+            [event.context?.params?.id]
+        )
+    }
 
     return response[0]
 })
