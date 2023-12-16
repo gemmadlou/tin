d82ff75d86043bc63e20f5e1e426637723cb5549
Gemma Black <gblackuk@googlemail.com>
2023-12-16 23:45:00 +0000

    [Prototype] create extracted file data view on uploads page

--- pages/uploads/[id].vue
+++ pages/uploads/[id].vue
@@ -10,61 +10,117 @@
 
         <div class="h-10"></div>
 
-        <div class="max-w-4xl grid grid-cols-4 gap-10">
-            <div class="col-span-1 flex items-center justify-center bg-gray-100 rounded-lg">
-                <div class="flex items-center">
-                    <div class="mr-3 uppercase font-bold">Step</div>
-                    <div class="bg-black text-white rounded-full w-12 h-12 flex items-center justify-center">
-                        1
+        <div class="grid grid-cols-5 gap-10">
+            <div class="col-span-2">
+                <div class="max-w-4xl grid grid-cols-5 gap-5 bg-gray-50 p-5">
+                    <div class="col-span-2 flex items-center justify-center bg-gray-200 rounded-lg">
+                        <div class="flex items-center m-3">
+                            <div class="mr-3 uppercase font-bold">Step</div>
+                            <div class="bg-black text-white rounded-full w-12 h-12 flex items-center justify-center">
+                                1
+                            </div>
+                        </div>
                     </div>
+                    <div class="col-span-3">
+                        <h2 class="font-bold uppercase mb-5">
+                            Upload File
+                        </h2>
+                        <form v-if="upload.form.id" class="mb-10" @submit.prevent="removeFile">
+                            <div class="overflow-x-auto">
+                                <table class="table">
+                                    <tbody>
+                                        <tr>
+                                            <th>{{ link.id }}</th>
+                                            <td>{{ upload.form.filename }}</td>
+                                            <td>
+                                                <button class="btn btn-neutral">
+                                                    Extract
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </form>
+                        <form v-else class="flex" @submit.prevent="uploadFile">
+                            <div class="mr-3 form-control w-full max-w-xs">
+                                <label class="label">
+                                    <span class="label-text">Pick a data file eg. csv</span>
+                                </label>
+                                <input type="file" v-on:change="onFileChange"
+                                    class="file-input file-input-bordered w-full max-w-xs" />
+                            </div>
+                            <div class="self-end">
+                                <button type="submit" class="btn">Upload</button>
+                            </div>
+                        </form>
+                        <div v-if="upload.error" class="text-red-500">{{ upload.error }}</div>
+                        <div v-if="upload.success" class="text-green-500">{{ upload.success }}</div>
+                    </div>
+                </div>
+
+                <div class="h-10"></div>
+
+                <div class="max-w-4xl grid grid-cols-5 gap-5 bg-gray-50 p-5">
+                    <div class="col-span-2 flex items-center justify-center bg-gray-200 rounded-lg">
+                        <div class="flex items-center m-3">
+                            <div class="mr-3 uppercase font-bold">Step</div>
+                            <div class="bg-black text-white rounded-full w-12 h-12 flex items-center justify-center">
+                                2
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-span-3">
+                        <h2 class="font-bold uppercase mb-5">
+                            Map file
+                        </h2>
+                    </div>
+                </div>
+
+                <div class="h-10"></div>
+
+                <div>
+                    Step 3 - Import file
                 </div>
             </div>
             <div class="col-span-3">
 
-                <form v-if="upload.form.id" class="mt-10 mb-10" @submit.prevent="removeFile">
-                    <div class="overflow-x-auto">
-                        <table class="table">
-                            <tbody>
-                                <tr>
-                                    <th>{{ link.id }}</th>
-                                    <td>{{ upload.form.filename }}</td>
-                                    <td>
-                                        <button class="btn">
-                                            ❌
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </form>
-                <form v-else class="flex" @submit.prevent="uploadFile">
-                    <div class="mr-3 form-control w-full max-w-xs">
-                        <label class="label">
-                            <span class="label-text">Pick a data file eg. csv</span>
-                        </label>
-                        <input type="file" v-on:change="onFileChange"
-                            class="file-input file-input-bordered w-full max-w-xs" />
-                    </div>
-                    <div class="self-end">
-                        <button type="submit" class="btn">Upload</button>
-                    </div>
-                </form>
-                <div v-if="upload.error" class="text-red-500">{{ upload.error }}</div>
-                <div v-if="upload.success" class="text-green-500">{{ upload.success }}</div>
+                <h2 class="font-bold uppercase mb-5">
+                    Extracted file data
+                </h2>
+
+                <table class="table table-zebra">
+                    <thead>
+                        <tr>
+                            <td>Heading A</td>
+                            <td>Heading A</td>
+                            <td>Heading A</td>
+                            <td>Heading A</td>
+                            <td>Heading A</td>
+                            <td>Heading A</td>
+                            <td>Heading A</td>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <td>1</td>
+                        <td>0</td>
+                        <td>John</td>
+                        <td>Smith</td>
+                        <td>22</td>
+                        <td>United Kingdom</td>
+                        <td>44 Lincoln Street, A11 1AA</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>Jane</td>
+                        <td>Blogs</td>
+                        <td>30</td>
+                        <td>France</td>
+                        <td>20 Paris Lane, A11 1AA</td>
+                    </tr>
+                </table>
             </div>
-        </div>
-
-        <div class="h-10"></div>
-
-        <div>
-            Step 2 - Map file
-        </div>
-
-        <div class="h-10"></div>
-
-        <div>
-            Step 3 - Import file
         </div>
     </div>
 </template>
@@ -148,7 +204,7 @@ const getUploadFile = async (uploadId: string | number) => {
 }
 
 const removeFile = async () => {
-    
+
 }
 
 const setSchemaInfo = async () => {
