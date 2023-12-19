acce714872b6e40432378b66f86f33a90d6fabb7
Gemma Black <gblackuk@googlemail.com>
2023-12-19 09:09:17 +0000

    [Prototype] create [GET] mapper/<id> endpoint

---
+++ server/api/mappers/[id]/index.get.ts
@@ -0,0 +1,17 @@
+import { connection } from "~/src/mysql"
+
+
+// @todo dedupe in map/csv.ts
+export default defineEventHandler(async (event) => {
+    let conn = await connection();
+
+    let [data] = await conn.query(
+        'select * from `mappers` where id = ?',
+        [event.context?.params?.id]
+    )
+
+    return {
+       ...data[0],
+       config: JSON.parse(data[0].mapper_config)
+    }
+})
