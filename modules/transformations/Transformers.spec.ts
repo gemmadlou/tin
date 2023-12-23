83eb2f73299b24cf1a1875783a705d02245ec56b
Gemma Black <gblackuk@googlemail.com>
2023-12-23 15:00:15 +0000

    [Prototype] add code coverage

--- modules/transformations/Transformers.spec.ts
+++ modules/transformations/Transformers.spec.ts
@@ -1,62 +1,97 @@
 // @vitest-environment nuxt
-import { expect, test } from 'vitest'
+import { expect, test, describe } from 'vitest'
 import { type Data, mapDataHeadingsToSchemaHeadings, type Mapper } from './Transformer'
 
-test('Feature: Map schema headings to multiple extracted data fields', () => {
-    let expected = {
-        name: [ "John", "Smith" ],
-        age: [22],
-        address: ["44 Lincoln Street, A11 1AA", "United Kingdom"],
-        signed_up: ["11 December 2023"]
-    }
+describe("Feature: Map schema headings to multiple extracted data fields", () => {
+    test('Expected Case:', () => {
+        let expected = {
+            name: [ "John", "Smith" ],
+            age: [22],
+            address: ["44 Lincoln Street, A11 1AA", "United Kingdom"],
+            signed_up: ["11 December 2023"]
+        }
+        
+        let dataInput = new Set<Data>([
+            {
+                dataHeading: "First name",
+                dataValue: "John"
+            },
+            {
+                dataHeading: "Last name",
+                dataValue: "Smith"
+            },
+            {
+                dataHeading: "Age",
+                dataValue: 22
+            },
+            {
+                dataHeading: "Country",
+                dataValue: "United Kingdom"
+            },
+            {
+                dataHeading: "Address",
+                dataValue: "44 Lincoln Street, A11 1AA"
+            },
+            {
+                dataHeading: "Joined",
+                dataValue: "11 December 2023"
+            }
+        ])
     
-    let dataInput = new Set<Data>([
-        {
-            dataHeading: "First name",
-            dataValue: "John"
-        },
-        {
-            dataHeading: "Last name",
-            dataValue: "Smith"
-        },
-        {
-            dataHeading: "Age",
-            dataValue: 22
-        },
-        {
-            dataHeading: "Country",
-            dataValue: "United Kingdom"
-        },
-        {
-            dataHeading: "Address",
-            dataValue: "44 Lincoln Street, A11 1AA"
-        },
-        {
-            dataHeading: "Joined",
-            dataValue: "11 December 2023"
+        let mapper = new Set<Mapper>([
+            {
+                schemaHeading: "name",
+                dataHeadings: [ "First name", "Last name" ]
+            },
+            {
+                schemaHeading: "age",
+                dataHeadings: [ "Age" ]
+            },
+            {
+                schemaHeading: "address",
+                dataHeadings: [ "Address", "Country" ]
+            },
+            {
+                schemaHeading: "signed_up",
+                dataHeadings: [ "Joined" ]
+            }
+        ])
+    
+        let actual = mapDataHeadingsToSchemaHeadings(mapper, dataInput)
+    
+        expect(actual).toEqual(expected)
+    })
+    
+    test('Edge Case: Don\'t map schema headings when data value does not exist', () => {
+        let expected = {
+            name: [ "John", "Smith" ]
         }
-    ])
-
-    let mapper = new Set<Mapper>([
-        {
-            schemaHeading: "name",
-            dataHeadings: [ "First name", "Last name" ]
-        },
-        {
-            schemaHeading: "age",
-            dataHeadings: [ "Age" ]
-        },
-        {
-            schemaHeading: "address",
-            dataHeadings: [ "Address", "Country" ]
-        },
-        {
-            schemaHeading: "signed_up",
-            dataHeadings: [ "Joined" ]
-        }
-    ])
-
-    let actual = mapDataHeadingsToSchemaHeadings(mapper, dataInput)
-
-    expect(actual).toEqual(expected)
+    
+        let dataInput = new Set<Data>([
+            {
+                dataHeading: "First name",
+                dataValue: "John"
+            },
+            {
+                dataHeading: "Last name",
+                dataValue: "Smith"
+            }
+        ])
+    
+        let mapper = new Set<Mapper>([
+            {
+                schemaHeading: "name",
+                dataHeadings: [ "First name", "Last name" ]
+            },
+            {
+                schemaHeading: "age",
+                dataHeadings: [ "Age" ]
+            }
+        ])
+    
+        let actual = mapDataHeadingsToSchemaHeadings(mapper, dataInput)
+    
+        expect(actual).toEqual(expected)
+        
+    })
 })
