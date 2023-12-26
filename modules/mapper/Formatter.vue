de88acb90854065e6f96a8c6ce4750961685339f
Gemma Black <gblackuk@googlemail.com>
2023-12-26 14:50:09 +0000

    [Prototype] complete Formatter component state functionality

--- modules/mapper/Formatter.vue
+++ modules/mapper/Formatter.vue
@@ -1,31 +1,25 @@
 <template>
-    {{  formatter._state }}
     <div v-if="(formatter.state() instanceof Machine.ClosedUnformatted)
-            || (formatter.state() instanceof Machine.ClosedFormatted)"
-        class="flex">
-        <button  
-            class="btn btn-neutral" 
-            v-on:click="open">Open</button>
-        
-        <div v-if="(formatter.state() instanceof Machine.ClosedFormatted)"
-            class="w-5 h-5 bg-gray-300 block">
+        || (formatter.state() instanceof Machine.ClosedFormatted)" class="flex">
+        <button class="btn rounded-full text-4xl" v-on:click="open">📄</button>
+
+        <div v-if="(formatter.state() instanceof Machine.ClosedFormatted)" class="w-5 h-5 bg-gray-300 block">
             ↗️
         </div>
     </div>
 
     <select v-if="(formatter.state() instanceof Machine.OpenUnformatted)
-                || (formatter.state() instanceof Machine.OpenFormatted)"
-            v-on:change="selectFormat">
+        || (formatter.state() instanceof Machine.OpenFormatted)"
+        class="select select-bordered w-full max-w-xs mr-5"
+        v-on:change="selectFormat">
         <option>Select option</option>
-        <option>Date</option>
+        <option v-for="(option, name) of options">{{ name }}</option>
         <option>Email</option>
     </select>
 
-    <button
-        v-if="(formatter.state() instanceof Machine.OpenFormatted)
-            || (formatter.state() instanceof Machine.OpenUnformatted)"
-        class="btn btn-neutral"
-        v-on:click="close">Close</button>
+    <button v-if="(formatter.state() instanceof Machine.OpenFormatted)
+        || (formatter.state() instanceof Machine.OpenUnformatted)" class="btn rounded-full"
+        v-on:click="close">❌</button>
 </template>
 
 <script setup lang="ts">
@@ -33,6 +27,7 @@ import * as Machine from "./Formatter";
 import { ref } from "vue"
 
 let formatter = ref(new Machine.Formatter)
+let options = Machine.Format
 
 const open = () => {
     formatter.value = formatter.value.state().open()
@@ -43,7 +38,11 @@ const close = () => {
 }
 
 const selectFormat = (e) => {
-    formatter.value = formatter.value.state().selectFormat(e.target.value)
+    if (Object.keys(Machine.Format).includes(e.target.value)) {
+        formatter.value = formatter.value.state().selectFormat(e.target.value)
+    } else {
+        formatter.value = formatter.value.state().removeFormat()
+    }
 }
 
 </script>
