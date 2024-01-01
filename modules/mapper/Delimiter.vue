9f5ba41fc447b418b90284f120ce3af04c37890b
Gemma Black <gblackuk@googlemail.com>
2024-01-01 11:35:49 +0000

    [Prototype] show delimited field in DataHeading modal

--- modules/mapper/Delimiter.vue
+++ modules/mapper/Delimiter.vue
@@ -1,25 +1,10 @@
 <template>
-    <div v-if="(delimiter.state() instanceof Machine.ClosedDelimited)
-        || (delimiter.state() instanceof Machine.ClosedUndelimited)" class="flex">
-        <button class="btn rounded-full text-4xl text-white" v-on:click="open">➗</button>
-
-        <div v-if="(delimiter.state() instanceof Machine.ClosedDelimited)"
-            class="w-5 h-5 bg-gray-300 rounded-full flex items-center justify-center -ml-3">
-            ↗️
-        </div>
-    </div>
-    <div class="flex items-center" 
-        v-if="delimiter.state() instanceof Machine.OpenUndelimited
-        || delimiter.state() instanceof Machine.OpenDelimited
-        ">
+    <div class="flex items-center">
         <input type="text" class="input input-bordered w-16" v-model="delimiter.delimiter" v-on:change="delimiterUpdate" />
         <select class="select select-bordered w-20 mr-2" v-on:change="selectDelimiterIndex"
             :disabled="delimiter.choiceOfIndices === undefined">
             <option v-for="choice in delimiter.choiceOfIndices">{{ choice }}</option>
         </select>
-        <button class="bg-gray-100 rounded-full p-0 w-10 h-10 text-2xl" v-on:click="close">
-            ➖
-        </button>
     </div>
     <input v-if="delimiter.state() instanceof Machine.OpenDelimited && delimiter.selectedText" type="text" disabled :value="delimiter.selectedText"
         class="input input-bordered input-xs w-25 text-center" />
@@ -30,14 +15,6 @@ import * as Machine from "./Delimiter";
 import { ref } from "vue";
 
 let delimiter = ref(new Machine.Delimiter("Stark Tower, Manhattan, New York, United States"))
-
-const open = () => {
-    delimiter.value = delimiter.value.state().open()
-}
-
-const close = () => {
-    delimiter.value = delimiter.value.state().close()
-}
 
 const delimiterUpdate = (event) => {
     if (event.target.value) {
