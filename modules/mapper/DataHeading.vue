ab86ad5968cb71b0c476355ed2e236cddf9e99f1
Gemma Black <gblackuk@googlemail.com>
2024-01-06 08:39:48 +0000

    [Prototype] add static mapping to DataHeading modal

--- modules/mapper/DataHeading.vue
+++ modules/mapper/DataHeading.vue
@@ -6,7 +6,8 @@
     <dialog class="modal" :style="modalStyle">
         <div v-on:click="hide" class="bg-transparent h-full w-full left-0 top-0 absolute pointer"></div>
         <div class="modal-box">
-            <h3 class="font-bold text-lg capitalize">{{ mappedField.schemaHeading }} >> {{ mappedField.dataHeadings[modelIndex] }}</h3>
+            <h3 class="font-bold text-lg capitalize">{{ mappedField.schemaHeading }} >> {{
+                mappedField.dataHeadings[modelIndex] }}</h3>
 
             <span class="block h-10"></span>
 
@@ -21,28 +22,47 @@
                 </div>
             </div>
 
-            <span class="block h-5"></span>
-
             <div v-if="typeof mappedField.dataHeadings[modelIndex] === 'object'
-                && 'headingName' in mappedField.dataHeadings[modelIndex]" class="flex items-center justify-stretch">
-                <div class="basis-32">
-                    Mapped to:
-                </div>
-                <div class="flex-1">
-                    <select v-model="mappedField.dataHeadings[modelIndex].headingName" class="select select-bordered w-full">
-                        <option v-for="(field) in uploadFields">{{ field }}</option>
-                    </select>
+                && 'headingName' in mappedField.dataHeadings[modelIndex]">
+                <span class="block h-5"></span>
+
+                <div class="flex items-center justify-stretch">
+                    <div class="basis-32">
+                        Mapped to:
+                    </div>
+                    <div class="flex-1">
+                        <select v-model="mappedField.dataHeadings[modelIndex].headingName"
+                            class="select select-bordered w-full">
+                            <option v-for="(field) in uploadFields">{{ field }}</option>
+                        </select>
+                    </div>
                 </div>
             </div>
 
-            <span class="block h-5"></span>
-            <div v-if="state.status === State.Delimited" class="flex items-center justify-stretch">
-                <div class="basis-32">
-                    Delimiter:
+            <div v-if="state.status === State.Delimited">
+                <span class="block h-5"></span>
+
+                <div class="flex items-center justify-stretch">
+                    <div class="basis-32">
+                        Delimiter:
+                    </div>
+                    <div class="flex-1">
+                        <Delimiter :data-heading="mappedField.dataHeadings[modelIndex]" />
+                    </div>
                 </div>
-                <div class="flex-1">
-                    <Delimiter 
-                        :data-heading="mappedField.dataHeadings[modelIndex]" />
+            </div>
+
+            <div v-if="state.status === State.Static">
+                <span class="block h-5"></span>
+
+                <div class="flex items-center justify-stretch">
+                    <div class="basis-32">
+                        Input:
+                    </div>
+                    <div class="flex-1">
+                        <input placeholder="Enter static value" type="text" class="input input-bordered w-full"
+                            v-model="mappedField.dataHeadings[modelIndex].static" />
+                    </div>
                 </div>
             </div>
 
@@ -67,18 +87,11 @@ const emit = defineEmits(['update'])
 
 enum State {
     Simple = "simple",
+    Static = "static",
     Delimited = "delimited"
 }
 
-const useDelimited = () => ({
-    status: State.Delimited
-})
-
-const useSimple = () => ({
-    status: State.Simple
-})
-
-let state = ref(useSimple())
+let state = ref({ status: State.Simple })
 let modalStyle = ref({})
 
 const changeHeader = (event) => {
@@ -87,15 +100,22 @@ const changeHeader = (event) => {
 
 const updateFieldType = (asState: State) => {
     switch (asState) {
-        case State.Simple as string: 
-            state.value = useSimple();
+        case State.Simple as string:
+            state.value.status = State.Simple
             props.mappedField.dataHeadings[props.modelIndex] = {
                 type: "simple",
                 headingName: props.mappedField.dataHeadings[props.modelIndex].headingName || props.mappedField.dataHeadings[props.modelIndex]
             }
             break;
-        case State.Delimited as string: 
-            state.value = useDelimited(); 
+        case State.Static as string:
+            state.value.status = State.Static
+            props.mappedField.dataHeadings[props.modelIndex] = {
+                type: "static",
+                static: props.mappedField.dataHeadings[props.modelIndex]?.static || ""
+            }
+            break;
+        case State.Delimited as string:
+            state.value.status = State.Delimited
             props.mappedField.dataHeadings[props.modelIndex] = {
                 type: "delimited",
                 headingName: props.mappedField.dataHeadings[props.modelIndex].headingName || props.mappedField.dataHeadings[props.modelIndex],
