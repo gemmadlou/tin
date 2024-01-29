5292634236a4c5315dd6b31217f95af121f88a2b
Gemma Black <gblackuk@googlemail.com>
2024-01-29 22:37:16 +0000

    [Auth] Add account mgmt button

--- components/Header.vue
+++ components/Header.vue
@@ -2,6 +2,7 @@
     <div class="container mx-auto pt-5">
         <div class="navbar bg-base-100">
             <nuxt-link to="/" class="btn btn-ghost text-xl -ml-1">Tin</nuxt-link>
+            <a href="/session" class="ml-auto btn btn-neutral">Account</a>
         </div>
     </div>
 </template>
