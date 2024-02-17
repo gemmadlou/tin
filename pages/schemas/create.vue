d35a9f1e1112dd9b54a3fdaa4676e4686c2aa1ac
Gemma Black <gblackuk@googlemail.com>
2024-02-17 03:47:21 +0000

    [MVP] add new schema page - /schemas/create

---
+++ pages/schemas/create.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="text-4xl font-semibold leading-relaxed">Add new schema</h1>
+    <p class="text-lg text-slate-500 max-w-lg">
+        Define, import or upload your schema configuration.
+    </p>
+
+    <div class="h-10"></div>
+
+    <div class="rounded-sm overflow-hidden p-8 shadow-md shadow-purple-100 bg-purple-50">
+        Form will go here
+    </div>
+
+    <div class="h-10"></div>
+
+    <div class="text-right">
+        <button
+            class="shadow-lg shadow-purple-100 bg-gradient-to-r from-purple-500 to-blue-500 hover:from-purple-700 hover:to-blue-700 text-white p-8 pt-3 pb-3 rounded font-bold">
+            Save
+        </button>
+    </div>
+</template>
