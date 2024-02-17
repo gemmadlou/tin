d3905ebb2f71fe2aefe3b0e75e225eab88612a66
Gemma Black <gblackuk@googlemail.com>
2024-02-17 04:08:21 +0000

    [MVP] /schemas/create radio buttons toggling different ways to import
    - text area for manual JSON definitions
    - URL pointing
    - File uploads

--- pages/schemas/create.vue
+++ pages/schemas/create.vue
@@ -6,8 +6,53 @@
 
     <div class="h-10"></div>
 
-    <div class="rounded-sm overflow-hidden p-8 shadow-md shadow-purple-100 bg-purple-50">
-        Form will go here
+    <div class="text-slate-600 rounded-sm overflow-hidden p-8 shadow-md shadow-purple-100 bg-purple-50">
+        <label class="font-bold">
+            Create schema by:
+        </label>
+        <div class="grid grid-cols-3 gap-4 max-w-3xl">
+            <div class="flex items-center space-x-3 p-4 pl-0">
+                <input type="radio" name="options" class="h-4 w-4" checked />
+                <label class="block">
+                    Define manually
+                </label>
+            </div>
+            <div class="flex items-center space-x-3 p-4 pl-0">
+                <input type="radio" name="options" class="h-4 w-4" />
+                <label class="block">
+                    Upload file
+                </label>
+
+            </div>
+            <div class="flex items-center space-x-3 p-4 pl-0">
+                <input type="radio" name="options" class="h-4 w-4" />
+                <label class="block">
+                    Connect to URL
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="h-5"></div>
+
+    <div class="text-slate-600 rounded-sm overflow-hidden p-8 shadow-md shadow-purple-100 bg-purple-50">
+        <div>
+            <label class="block font-bold">
+                Schema name
+            </label>
+            <input type="text" name="schemaName"
+                class="w-96 border border-purple-500 h-10 px-4 rounded-lg text-sm focus:outline-none focus:border-purple-500">
+        </div>
+
+        <div class="h-5"></div>
+
+        <div>
+            <label for="schemaName" class="block font-bold">
+                Schema description
+            </label>
+            <textarea id="schemaName" name="schemaName" rows="4"
+                class="w-96 border border-purple-500 rounded-lg px-4 py-2 text-sm focus:outline-none focus:border-purple-500"></textarea>
+        </div>
     </div>
 
     <div class="h-10"></div>
