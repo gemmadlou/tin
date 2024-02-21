d2a1e9ed5f4e691acb346c14ae5508dee224040d
Gemma Black <gblackuk@googlemail.com>
2024-02-21 21:58:19 +0000

    [MVP] /schemas/create manual definition UI to API integration: save

--- pages/schemas/create.vue
+++ pages/schemas/create.vue
@@ -16,17 +16,18 @@
             <div class="flex items-center space-x-3 p-4 pl-0">
                 <input type="radio" name="options" class="h-4 w-4" checked />
                 <label class="block">
-                    Define manually
+                    JSON schema input
                 </label>
             </div>
-            <div class="flex items-center space-x-3 p-4 pl-0">
+
+            <div class="hidden items-center space-x-3 p-4 pl-0">
                 <input type="radio" name="options" class="h-4 w-4" />
                 <label class="block">
                     Upload file
                 </label>
-
             </div>
-            <div class="flex items-center space-x-3 p-4 pl-0">
+
+            <div class="hidden items-center space-x-3 p-4 pl-0">
                 <input type="radio" name="options" class="h-4 w-4" />
                 <label class="block">
                     Connect to URL
@@ -38,45 +39,21 @@
     <div class="h-5"></div>
 
     <div class="text-slate-600 rounded-sm overflow-hidden p-8 shadow-md shadow-purple-100 bg-purple-50">
+
         <div>
             <label class="block font-bold">
-                Schema name
-            </label>
-            <input type="text" name="schemaName"
-                class="w-96 border border-purple-500 h-10 px-4 rounded-lg text-sm focus:outline-none focus:border-purple-500">
-        </div>
-
-        <div class="h-5"></div>
-
-        <div>
-            <label for="schemaName" class="block font-bold">
-                Schema description
-            </label>
-            <div class="inline-grid grid-cols-2 space-x-4">
-                <div>
-                    <textarea name="schemaName" rows="3"
-                        class="w-96 border border-purple-500 rounded-lg px-4 py-2 text-sm focus:outline-none focus:border-purple-500"></textarea>
-                </div>
-                <div class="max-w-56 italic text-slate-400">
-                    Add a short description to help others understand what this schema configuration is for.
-                </div>
-            </div>
-        </div>
-
-        <div class="h-5"></div>
-
-
-        <div>
-            <label for="schemaName" class="block font-bold">
                 Schema configuration
             </label>
             <div class="inline-grid grid-cols-2 space-x-4">
                 <div>
-                    <textarea name="schemaConfig" rows="10" placeholder="{}"
-                        class="w-96 border border-purple-500 rounded-lg px-4 py-2 text-sm focus:outline-none focus:border-purple-500"></textarea>
+                    <textarea v-model="formData.json" rows="10" placeholder="{}"
+                        class="w-96 border border-purple-500 rounded-lg px-4 py-2 text-sm focus:outline-none focus:border-purple-500">
+                    </textarea>
                 </div>
-                <div class="max-w-56 italic text-slate-400">
+                <div class="max-w-64 italic text-slate-400">
                     Add JSON Schema in correct JSON format.
+                    View the documentations at
+                    <a href="https://json-schema.org/" target="_blank">json-schema.org</a>.
                 </div>
             </div>
         </div>
@@ -85,9 +62,60 @@
     <div class="h-10"></div>
 
     <div class="text-right">
-        <button
+        <button @click="submitForm"
             class="shadow-lg shadow-purple-100 bg-gradient-to-r from-purple-500 to-blue-500 hover:from-purple-700 hover:to-blue-700 text-white p-8 pt-3 pb-3 rounded font-bold">
             Save
         </button>
     </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const formData = ref({
+    json: ''
+});
+
+const isLoading = ref(false);
+
+const error = ref({ })
+const success = ref('')
+
+const submitForm = async () => {
+    try {
+
+        // Check if the form is already loading
+        if (isLoading.value) {
+            console.warn('Form is already submitting. Please wait.');
+            return;
+        }
+
+        // Start loading
+        isLoading.value = true;
+
+        // Send a POST request with the JSON data
+        const response = await fetch('/api/schemas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ json: JSON.parse(formData.value.json)}) // Send formData.value as JSON
+        });
+
+        if (response.ok) {
+            success.value = 'Form data successfully submitted!';
+            formData.value = { json: '' };
+        } else {
+            console.error('Failed to submit form data:', response.statusText);
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Error submitting form data:', error.message);
+        } else {
+            console.error('An unknown error occurred:', error);
+        }
+    } finally {
+        isLoading.value = false
+    }
+};
+</script>
