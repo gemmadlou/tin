f2aba8316ed25fc833a70cb1c565ffef2b16c47e
Gemma Black <gblackuk@googlemail.com>
2024-02-24 18:33:12 +0000

    [MVP] GET /api/schemas/:id UI to API integration

--- pages/schemas/[id].vue
+++ pages/schemas/[id].vue
@@ -1,37 +1,68 @@
 <template>
-    <div class="opacity-65 mb-5">Schemas > {{ name }}</div>
+    <div class="opacity-65 mb-5">Schemas > {{ schema.name }}</div>
 
     <h1 class="text-4xl font-semibold leading-relaxed">Schema Configuration</h1>
     <p class="text-lg text-slate-500 max-w-lg">
         View schema configuration object.
     </p>
-    
+
     <div class="h-5"></div>
 
     <div class="text-slate-600 rounded-sm overflow-hidden p-8 shadow-md shadow-purple-100 bg-purple-50">
-        <label class="block font-bold mb-5">Schema configuration</label>
+        <section class="mb-5 max-w-96">
+            <label class="block font-bold">ID:</label>
 
-        <div class="inline-grid grid-cols-2 space-x-4">
-            <div>
-                <textarea v-model="formData.json" rows="10" placeholder="{}"
-                    class="w-96 border border-purple-500 rounded-lg px-4 py-2 text-sm focus:outline-none focus:border-purple-500" 
+            <div class="inline-grid">
+                {{ schema.id }}
+            </div>
+        </section>
+        
+        <section class="mb-5 max-w-96">
+            <label class="block font-bold">Name:</label>
+
+            <div class="inline-grid">
+                {{ schema.name }}
+            </div>
+        </section>
+
+        <section class="mb-5 max-w-96">
+            <label class="block font-bold">Description:</label>
+
+            <div class="inline-grid">
+                {{ schema.description }}
+            </div>
+        </section>
+        
+        <section>
+            <label class="block font-bold mb-2">Schema configuration:</label>
+
+            <div class="inline-grid grid-cols-2 space-x-4">
+                <textarea v-model="schema.json" rows="10" placeholder="{}"
+                    class="w-96 bg-slate-50 border border-purple-500 rounded-lg px-4 py-2 text-sm focus:outline-none focus:border-purple-500"
                     readonly />
             </div>
-        </div>
-
+        </section>
         <div class="h-5"></div>
-
-        <div v-if="hasError">
-            <label class="block font-bold mb-5">Errors</label>
-            <div class="bg-red-200 p-6 pt-4 pb-4 rounded-lg">
-                <span class="block" v-for="i in error.map((i: any) => `${i.path.replace('$.json', '$')} ${i.error}`)">{{
-                    i }}</span>
-            </div>
-        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
 
-const formData = ref({ json: '' })
+const route = useRoute();
+const schemaId = route.params.id;
+
+const schema = ref({
+    id: '',
+    json: '',
+    name: '',
+    description: ''
+})
+
+let data = await $fetch(`/api/schemas/${schemaId}`)
+console.log({ data })
+if (data) {
+    schema.value = data
+    schema.value.json = JSON.stringify(data.json, null, 4)
+}
 </script>
