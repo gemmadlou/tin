75460b085fc3b130684679f0791aec9d1bd93271
Gemma Black <gblackuk@googlemail.com>
2024-02-25 17:34:22 +0000

    [MVP] Custom 404 page that's not the Nuxt default

---
+++ error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+    error: Object as () => NuxtError
+})
+</script>
+
+<template>
+    <NuxtLayout>
+        <h1 class="text-4xl font-semibold leading-relaxed">Error {{ error.statusCode }}</h1>
+        <div class="h-5"></div>
+        <NuxtLink to="/">
+            <FormButton>Go home</FormButton>
+        </NuxtLink>
+    </NuxtLayout>
+</template>
